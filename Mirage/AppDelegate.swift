6a00d7da19165ee12ce2fa36cbf845b4bd1f3f10
Jonathan Zrake <jonathan.zrake@gmail.com>
2018-08-27 11:39:17 -0400

    Initial Commit

---
+++ Mirage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mirage
+//
+//  Created by Jonathan Zrake on 8/27/18.
+//  Copyright © 2018 Jonathan Zrake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mirage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mirage
+//
+//  Created by Jonathan Zrake on 8/27/18.
+//  Copyright © 2018 Jonathan Zrake. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
