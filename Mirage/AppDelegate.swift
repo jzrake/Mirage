160694bcd84529d88e95ed3804e38b6e060c7ae5
Jonathan Zrake <jonathan.zrake@gmail.com>
2018-09-06 15:53:44 -0400

    Add pybind11 and embedded Python as git submodules

--- Mirage/AppDelegate.swift
+++ Mirage/AppDelegate.swift
@@ -10,7 +10,6 @@ class AppDelegate: NSObject, NSApplicationDelegate
     func applicationDidFinishLaunching(_ aNotification: Notification)
     {
         PythonRuntime.initializeInterpreter()
-        PythonRuntime.add(toSystemPath: URL(fileURLWithPath: "/Users/jzrake/Work/Mirage"))
     }
 
     func applicationWillTerminate(_ aNotification: Notification)
